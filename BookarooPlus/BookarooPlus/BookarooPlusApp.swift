5877e899ec0a6a9981d658bc0a264830949b177b
Patrik Michl <xmichl@mendelu.cz>
2024-01-01 19:14:52 +0100

    init

---
+++ BookarooPlus/BookarooPlus/BookarooPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookarooPlusApp.swift
+//  BookarooPlus
+//
+//  Created by Patrik Michl on 01.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookarooPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
