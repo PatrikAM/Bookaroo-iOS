6404230814293476bc60f0a2cefccbbe2648ed43
xglas <xglas@mendelu.cz>
2024-01-08 12:01:12 +0100

    baseurls

---
+++ BookarooPlus/BookarooPlus/Views/ListOfLibrariesView.swift
@@ -0,0 +1,14 @@
+//
+//  LibrariesView.swift
+//  BookarooPlus
+//
+//  Created by Patrik Michl on 08.01.2024.
+//
+
+import SwiftUI
+
+struct ListOfLibrariesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
---
+++ BookarooPlus/BookarooPlus/Views/ListOfReadersView.swift
@@ -0,0 +1,18 @@
+//
+//  ListOfReadersView.swift
+//  BookarooPlus
+//
+//  Created by Patrik Michl on 08.01.2024.
+//
+
+import SwiftUI
+
+struct ListOfReadersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListOfReadersView()
+}
