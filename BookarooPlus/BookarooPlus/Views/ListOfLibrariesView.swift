cc82fc3338eb232a665571d099c9d18e8087f764
Marek Glas <xglas@mendelu.cz>
2024-01-19 20:54:37 +0100

    started ui updates on library list
    added padding to outer vstack, added text camel casing, added dividers and spacers between foreach elements

--- BookarooPlus/BookarooPlus/Views/ListOfLibrariesView.swift
+++ BookarooPlus/BookarooPlus/Views/ListOfLibrariesView.swift
@@ -13,34 +13,55 @@ struct ListOfLibrariesView: View {
     @ObservedObject var viewModel = ListOfLibrariesViewModel()
     
     var body: some View {
-        VStack {
-            if (viewModel.isLoading) {
-                ProgressView()
-            } else {
-                ForEach(viewModel.libraries!) { lib in
-                    VStack {
-                        Text(lib.name!)
-                            .font(.largeTitle)
-                            .bold()
-                        var l = [Library(id: "fv", favourite: lib.favourite!), Library(id: "total", favourite: lib.total! - lib.favourite!)]
+        ScrollView {
+            VStack {
+                if (viewModel.isLoading) {
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                    // TODO: separate localized views for error handling from viewmodel error message
+                } else {
+                    ForEach(viewModel.libraries!) { lib in
                         VStack {
-                            Chart (l) { sector in
-                                SectorMark(
-                                    angle: .value(Text(verbatim:"Value"), sector.favourite!),
-                                    innerRadius: .ratio(0.6),
-                                    //                                                                outerRadius: .inset(10),
-                                    angularInset: 4
-                                )
-                                .cornerRadius(4)
-                                .foregroundStyle(sector.id == "fv" ? Color.blue : Color.red)
+                            Text(lib.name!.capitalizeEachWord())
+                                .font(.largeTitle)
+                                .bold()
+                            var l = [Library(id: "fv", favourite: lib.favourite!), Library(id: "total", favourite: lib.total! - lib.favourite!)]
+                            VStack {
+                                Chart (l) { sector in
+                                    SectorMark(
+                                        angle: .value(Text(verbatim:"Value"), sector.favourite!),
+                                        innerRadius: .ratio(0.6),
+                                        //                                                                outerRadius: .inset(10),
+                                        angularInset: 4
+                                    )
+                                    .cornerRadius(4)
+                                    .foregroundStyle(sector.id == "fv" ? Color.blue : Color.red)
+                                }
                             }
+                            .frame(maxWidth:100)
                         }
-                        .frame(maxWidth:100)
+                        
+                        Spacer()
+                            .frame(height: 30)
+                        
+                        ProgressView(value: Float(lib.read!) / Float(lib.total!),
+                                     label: { Text("Keep reading!") },
+                                     currentValueLabel: { Text("\(Float(lib.read!) / Float(lib.total!) *   100, specifier: "%.2f")%") })
+                        .progressViewStyle(ProgressBarStyle(height: 50))
+                        .frame(height: 60)
+                        
+                        Spacer()
+                            .frame(height: 40)
+                        
+                        if lib != viewModel.libraries!.last {
+                            Divider()
+                            Spacer()
+                                .frame(height: 60)
+                        }
                     }
-                    ProgressView(value: Float(lib.read!) / Float(lib.total!), label: { Text("Keep reading!") }, currentValueLabel: { Text("\(Float(lib.read!) / Float(lib.total!) *   100, specifier: "%.2f")%") })
-                        .progressViewStyle(ProgressBarStyle(height: 100.0))
                 }
             }
+            .padding(.all)
         }
         .onAppear {
             viewModel.fetchLibraries()
