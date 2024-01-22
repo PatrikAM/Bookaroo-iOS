9c3f802268446d54b419d83abbaf5b824c5de987
Patrik Michl <xmichl@mendelu.cz>
2024-01-22 22:17:44 +0100

    detail

---
+++ BookarooPlus/BookarooPlus/Elements/BookCard.swift
@@ -0,0 +1,103 @@
+//
+//  BookCard.swift
+//  BookarooPlus
+//
+//  Created by Patrik Michl on 22.01.2024.
+//
+
+import SwiftUI
+
+struct BookCard: View {
+    
+    var book: Book
+    
+    let color: Color
+    
+    var onHeartClick: () -> ()
+    var onBookClick: () -> ()
+    
+    let bookPlaceHolderImage = "https://www.marytribble.com/wp-content/uploads/2020/12/book-cover-placeholder.png"
+
+    var body: some View {
+        ZStack(alignment: .top) {
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/, style: .continuous)
+                .fill(color)
+                .shadow(radius: 5)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, idealHeight: 250, maxHeight: 250)
+            
+            ZStack(alignment: .top) {
+                HStack(alignment: .top) {
+                    Button(action: {
+                        onBookClick()
+                    }) {
+                        Image(systemName: "book.circle")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .foregroundStyle(self.book.read ?? false ? .indigo : .white)
+                            .backgroundStyle(.brown)
+                            .clipShape(Circle())
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .frame(width: 100, height: 350)
+                    Spacer()
+                    Spacer()
+                    Button(action: {
+                        onHeartClick()
+                    }) {
+                        Image(systemName: "heart.circle")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .foregroundStyle(self.book.favourite ?? false ? .red : .white)
+                            .backgroundStyle(.brown)
+                            .clipShape(Circle())
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .frame(width: 100, height: 350)
+                }
+                VStack {
+                    VStack(alignment: .center) {
+                        Text(book.title ?? "Book title unknown")
+                            .font(.title3)
+                        Text(book.author ?? "Book author unknown")
+                            .font(.subheadline)
+                        Text("Pages: \(book.pages ?? 0)")
+                            .font(.subheadline)
+                    }
+//                    .padding(.top)
+                    //                Spacer()
+                    HStack {
+                        Spacer()
+                        if book.cover != nil && book.cover!.isValidUrl() {
+                            AsyncImage(url: URL(string: book.cover!)) { image in
+                                image.resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .clipShape(RoundedRectangle(cornerSize: CGSize(width: 20, height: 20)))
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(height: 300)
+                            .clipped()
+                            Spacer()
+                        } else {
+                            AsyncImage(url: URL(string: bookPlaceHolderImage)) { image in
+                                image.resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .clipShape(RoundedRectangle(cornerSize: CGSize(width: 20, height: 20)))
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(height: 300)
+                            .clipped()
+                            Spacer()
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: 400)
+                
+            }
+        }
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: 400)
+        .background()
+        
+    }
+}
--- BookarooPlus/BookarooPlus/Views/BookDetailView.swift
+++ BookarooPlus/BookarooPlus/Views/BookDetailView.swift
@@ -22,56 +22,131 @@ struct BookDetailView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                if viewModel.isLoading {
-                    ProgressView()
-                } else if viewModel.book != nil {
-                    Text(viewModel.book!.title!)
-                    Text(viewModel.book?.cover ?? "unknown")
-                    Text(viewModel.book?.isbn ?? "")
-                    if let image = viewModel.book?.cover {
+            GeometryReader { geometry in
+                ZStack(alignment: .top) {
+                    if let _ = viewModel.book?.cover {
                         if let image = viewModel.coverImage {
-                            Circle()
-                                .foregroundColor(Color(image.averageColor!))
+                            //                        Color(image.averageColor!)
+                            //                        //                        .frame(height: 100, alignment: .top)
+                            //                            .edgesIgnoringSafeArea(.top)
+                            Rectangle()
+                                .fill(Color(image.averageColor!))
+                                .frame(width: geometry.size.width * 1.4, height: geometry.size.height * 0.2)
+                                .position(x: geometry.size.width / 2.35, y: geometry.size.height * 0.1)
+                                .edgesIgnoringSafeArea(.all)
+                            
                         }
                         
                     }
-                    Text("TODO: info o knize")
-                    Button(action: {
-                        viewModel.deleteBook(bookId: bookId)
-                    }) {
-                        Text("Delete")
+                    
+                    VStack(alignment: .center) {
+                        if viewModel.isLoading {
+                            ProgressView()
+                        } else if viewModel.book != nil {
+                            BookCard(
+                                book: viewModel.book!,
+                                color: Color(viewModel.coverImage?.averageColor ?? .gray),
+                                onHeartClick: { viewModel.book?.favourite?.toggle() },
+                                onBookClick: { viewModel.book?.read?.toggle() }
+                            )
+//                            Text(viewModel.book!.title!)
+//                            Text(viewModel.book?.cover ?? "unknown")
+//                            Text(viewModel.book?.isbn ?? "")
+//                            if let image = viewModel.book?.cover {
+//                                if let image = viewModel.coverImage {
+//                                    Circle()
+//                                        .foregroundColor(Color(image.averageColor!))
+//                                }
+//                                
+//                            }
+                            
+                            Spacer()
+                            
+                            HStack {
+                                Text("Author")
+                                Spacer()
+                                Text("\(viewModel.book?.author ?? "unknown")")
+                            }
+                            Divider()
+                            
+                            HStack {
+                                Text("Title")
+                                Spacer()
+                                Text("\(viewModel.book?.title ?? "unknown")")
+                            }
+                            Divider()
+                            
+                            HStack {
+                                Text("Subtitle")
+                                Spacer()
+                                Text("\(viewModel.book?.subtitle ?? "unknown")")
+                            }
+                            Divider()
+                            
+                            HStack {
+                                Text("Pages")
+                                Spacer()
+                                Text("\(viewModel.book?.pages ?? 0)")
+                            }
+                            Divider()
+                            
+                            HStack {
+                                Text("ISBN")
+                                Spacer()
+                                Text("\(viewModel.book?.isbn ?? "unknown")")
+                            }
+                            Divider()
+                            
+                            HStack {
+                                Text("Published")
+                                Spacer()
+                                Text("\(viewModel.book?.published ?? "unknown")")
+                            }
+                            Divider()
+                            
+                            HStack {
+                                Text("Publisher")
+                                Spacer()
+                                Text("\(viewModel.book?.publisher ?? "unknown")")
+                            }
+                            Divider()
+                            
+                            Button(action: {
+                                viewModel.deleteBook(bookId: bookId)
+                            }) {
+                                Text("Delete")
+                            }
+                            
+                            Button(action: {
+                                editing = true
+                            }) {
+                                Text("Edit")
+                            }
+                        }
                     }
                     
-                    Button(action: {
-                        editing = true
-                    }) {
-                        Text("Edit")
+                    .navigationDestination(isPresented: $editing) {
+                        if viewModel.book != nil {
+                            BookAddEditView(book: viewModel.book!)
+                        }
                     }
-                    
+                    .toast(isPresenting: $viewModel.showError) {
+                        AlertToast(
+                            displayMode: .alert,
+                            type: .error(Color.red),
+                            subTitle: viewModel.errorMessage
+                        )
+                    }
+                    .onAppear {
+                        viewModel.fetchBook(bookId: bookId)
+                    }
+                    .onChange(of: viewModel.deletionDone) {
+                        deletedId = bookId
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                    .toolbar(.hidden, for: .tabBar)
                 }
             }
-            
-            .navigationDestination(isPresented: $editing) {
-                if viewModel.book != nil {
-                    BookAddEditView(book: viewModel.book!)
-                }
-            }
-            .toast(isPresenting: $viewModel.showError) {
-                AlertToast(
-                    displayMode: .alert,
-                    type: .error(Color.red),
-                    subTitle: viewModel.errorMessage
-                )
-            }
-            .onAppear {
-                viewModel.fetchBook(bookId: bookId)
-            }
-            .onChange(of: viewModel.deletionDone) {
-                deletedId = bookId
-                presentationMode.wrappedValue.dismiss()
-            }
-            .toolbar(.hidden, for: .tabBar)
         }
     }
 }
